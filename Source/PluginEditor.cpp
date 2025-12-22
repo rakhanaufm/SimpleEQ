f1db91b4a2cb35cc5e36f5211f17268db9d5ee9b
rakhanaufm <rakhamnaufal@gmail.com>
2025-12-23 02:16:41 +0700

    Added Analyzer Decibel Marks
    Cleaned up Code

--- Source/PluginEditor.cpp
+++ Source/PluginEditor.cpp
@@ -232,8 +232,7 @@ void ResponseCurveComponent::paint(juce::Graphics& g)
 
     g.drawImage(background, getLocalBounds().toFloat());
 
-    /*auto responseArea = getLocalBounds();*/
-    auto responseArea = getAnalysisArea(); //getRenderArea();
+    auto responseArea = getAnalysisArea(); 
 
     auto w = responseArea.getWidth();
 
@@ -329,12 +328,9 @@ void ResponseCurveComponent::resized()
     }
 
     g.setColour(Colours::dimgrey);
-    /*for (auto f : freqs)*/
     for (auto x :xs)
     {
-        /*auto normX = mapFromLog10(f, 20.f, 20000.f);*/
 
-        /*g.drawVerticalLine(getWidth() * normX, 0.f, getHeight());*/
         g.drawVerticalLine(x, top, bottom);
     }
 
@@ -346,12 +342,9 @@ void ResponseCurveComponent::resized()
     for (auto gDb : gain)
     {
         auto y = jmap(gDb, -24.f, 24.f, float(bottom), float(top));
-        /*g.drawHorizontalLine(y, 0, getWidth());*/
         g.setColour(gDb == 0.f ? Colour(187u, 154u, 247u) : Colours::darkgrey);
         g.drawHorizontalLine(y, left, right);
     }
-
-    //g.drawRect(getAnalysisArea());
 
     g.setColour(Colours::lightgrey);
     const int fontHeight = 10;
@@ -404,6 +397,15 @@ void ResponseCurveComponent::resized()
         g.setColour(gDb == 0.f ? Colour(187u, 154u, 247u) : Colours::lightgrey);
 
         g.drawFittedText(str, r, juce::Justification::centred, 1);
+
+        str.clear();
+        str << (gDb - 24.f);
+
+        r.setX(1);
+        textWidth = g.getCurrentFont().getStringWidth(str);
+        r.setSize(textWidth, fontHeight);
+        g.setColour(Colours::lightgrey);
+        g.drawFittedText(str, r, juce::Justification::centred, 1);
     }
 }
 
@@ -411,9 +413,6 @@ juce::Rectangle<int> ResponseCurveComponent::getRenderArea()
 {
     auto bounds = getLocalBounds();
 
-    //bounds.reduce(11, //JUCE_LIVE_CONSTANT(11), 
-    //    9 //JUCE_LIVE_CONSTANT(9)
-    //    ); 
     bounds.removeFromTop(12);
     bounds.removeFromBottom(2);
     bounds.removeFromLeft(20);
